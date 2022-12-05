c3e8cf869f2e241977cd0181cd025cdeb38dc1b3
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2022-12-05 13:35:38 +0000

    objects finished

--- src/arrays.js
+++ src/arrays.js
@@ -7,58 +7,60 @@ const getNthElement = (index, array) => {
 
 const arrayToCSVString = array => {
   return array.toString();
+  /* return array.join() */
 };
 
 const csvStringToArray = string => {
-  return string.addToArray();
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(strings => strings.toUpperCase());
 };
 
-const reverseWordsInArray = strings => {
-  // your code here
-};
+function reverseWordsInArray(strings) {
+  return strings.map(strings => strings.split('').reverse().join(''));
+  /*return strings.map(strings => [...strings].reverse().join(''));*/
+}
 
 const onlyEven = numbers => {
-  // your code here
+ return numbers.filter(numbers => numbers % 2 == 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  return array.filter(item => item !== index);
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+ return strings.filter(strings => /^[aeiou]/i.test(strings));
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.replace(/\s/g, '');
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  return numbers.reduce((total, item) => total + item);
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  return strings.sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
 };
 
 module.exports = {
